5ddde9490339dc979d74473e7c660ee0c5c60a28
Esraa Ehab <esraa.ehab105@gmail.com>
2025-11-01 15:07:03 +0200

    initializated tables

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+USE master; 
+
+IF EXISTS(SELECT 1 from sys.databases WHERE name = 'DataWareHouse')
+BEGIN
+    ALTER DATABASE DataWareHouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+    DROP DATABASE DataWareHouse
+END
+
+CREATE DATABASE DataWareHouse;
+
+USE DataWareHouse; 
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
