05d54790339e3cc477dd391ba95ebc42e73d0e32
Esraa Ehab <esraa.ehab105@gmail.com>
2025-11-29 22:52:12 +0200

    orchestrate silver layer

--- pipeline_scripts/silver_layer/run_all_transformations.sql
+++ pipeline_scripts/silver_layer/run_all_transformations.sql
@@ -1,15 +1,17 @@
 PRINT '==================================================================';
 PRINT ' Intiating data transformation - loading data in the silver layer';
 PRINT '==================================================================';
+PRINT '>> Initializing tables';
+:r ./pipeline_scripts/silver_layer/init_tables.sql
 PRINT 'Transforming and lading CRM customer data' ;
-:r ./scripts/silver_layer/crm_explore_transform/customer_info_transformation.sql
+:r ./pipeline_scripts/silver_layer/crm_explore_transform/customer_info_transformation.sql
 PRINT 'Transforming and lading CRM product data';
-:r ./scripts/silver_layer/crm_explore_transform/product_info_transformation.sql
+:r ./pipeline_scripts/silver_layer/crm_explore_transform/product_info_transformation.sql
 PRINT 'Transforming and lading CRM sales data';
-:r ./scripts/silver_layer/crm_explore_transform/sales_info_transformation.sql
+:r ./pipeline_scripts/silver_layer/crm_explore_transform/sales_info_transformation.sql
 PRINT 'Transforming and lading ERP customer data';
-:r ./scripts/silver_layer/erp_explore_transform/customer_az_transform.sql
+:r ./pipeline_scripts/silver_layer/erp_explore_transform/customer_az_transform.sql
 PRINT 'Transforming and lading ERP location data';
-:r ./scripts/silver_layer/erp_explore_transform/erp_location_cat_transform.sql
+:r ./pipeline_scripts/silver_layer/erp_explore_transform/erp_location_cat_transform.sql
 PRINT 'Transforming and lading ERP product-category data';
-:r ./scripts/silver_layer/erp_explore_transform/erp_cat_transform.sql
+:r ./pipeline_scripts/silver_layer/erp_explore_transform/erp_cat_transform.sql
