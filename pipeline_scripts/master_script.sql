6d851e2992a9452067163e3354ffe6f64c53dd53
Esraa Ehab <esraa.ehab105@gmail.com>
2025-11-29 22:52:43 +0200

    add master script to run the entire pipeline

--- pipeline_scripts/master_script.sql
+++ pipeline_scripts/master_script.sql
@@ -1,8 +1,41 @@
+-- ========================================================
+-- Master ETL Driver Script
+-- ========================================================
+DECLARE @startStep DATETIME2, @endStep DATETIME2;
+SET @startStep = SYSDATETIME();
+
 PRINT '================================================';
-PRINT '    Starting full data warehouse build process   ';
+PRINT ' Starting full data warehouse build process';
 PRINT '================================================';
+
+PRINT '----------------------------'
+PRINT 'Step 1: initialize database'
+PRINT '----------------------------'
+:r ./pipeline_scripts/init_database.sql
 GO
-:r ./scripts/bronze_layer/load_data_from_csv.sql
-:r ./scripts/silver_layer/run_all_transformations.sql
-:r ./scripts/gold_layer/create_gold_views.sql
-PRINT 'All layers built successfully.';
+
+PRINT '---------------------'
+PRINT 'Step 2: Bronze Layer'
+PRINT '---------------------'
+PRINT '>> Loading Bronze layer';
+:r ./pipeline_scripts/bronze_layer/excute_bronze_layer.sql
+GO
+
+PRINT '---------------------'
+PRINT 'Step 3: Silver Layer'
+PRINT '---------------------'
+PRINT '>> Running Silver layer transformations';
+:r ./pipeline_scripts/silver_layer/run_all_transformations.sql
+GO
+
+PRINT '-------------------'
+PRINT 'Step 4: Gold Layer'
+PRINT '-------------------'
+PRINT '>> Creating Gold layer views';
+:r ./pipeline_scripts/gold_layer/create_gold_views.sql
+GO
+
+PRINT '=================================';
+PRINT ' All layers built successfully';
+PRINT '=================================';
+GO
